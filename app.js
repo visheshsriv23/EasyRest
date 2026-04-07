26b3eee19e3862d9fce4fefe83c7e11fe75b1d68
Mac <mac@Vishesh-Device.local>
2026-04-07 13:56:07 +0530

    fix:Fixed an error

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ store.on("error", () => {
 
 const sessionOptions = {
 	store,
-	secret: process.env.SECRET,, 
+	secret: process.env.SECRET,
 	resave: false, 
 	saveUninitialized: true,
 	cookie: {
